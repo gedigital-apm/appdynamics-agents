2f4a200d23b94915fa3e4a2ab30b1988810f17f1
Tran, Dewey (GE Digital) <dewey@ge.com>
2018-11-27 13:44:53 -0800

    script to automate repacking config file

---
+++ repackage/README.md
@@ -0,0 +1,5 @@
+To use:
+1. Add versions to `targets.txt`.
+2. Run `./repackage.sh`
+3. Move contents of `dist` into root directory of the repo.
+4. Commit changes.
---
+++ repackage/repackage.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+destination="https://raw.githubusercontent.com/gedigital-apm/appdynamics-agents/master"
+
+output_dir="./dist"
+index_filename="index.yml"
+
+rm -rf "$output_dir"
+
+curl --create-dirs -o "$output_dir/$index_filename" -sSL "https://packages.appdynamics.com/java/index.yml" -H "user-agent: Ruby" -H "accept-encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3" -H "accept: /"
+
+while IFS='' read -r target || [[ -n "$target" ]]; do
+
+    echo "Processing target '$target'"
+
+    targetUrl=`sed -n -e "/^$target:/p" $output_dir/$index_filename`
+    targetUrl="${targetUrl//$target: /}"
+
+    if [ "$targetUrl" == '' ]
+    then
+        echo "    ERROR: TARGET '$target'. Skipping..."
+        continue
+    fi
+
+    agent_filename="${targetUrl##*/}"
+    echo "    Downloading $agent_filename from $targetUrl"
+    curl --create-dirs -o "$output_dir/$agent_filename" -sSL "$targetUrl"
+    echo "    Done downloading"
+
+    agent_folder=$(echo $agent_filename | rev | cut -f 2- -d '.' | rev)
+    echo "    Unzipping '$output_dir/$agent_filename' to '$output_dir/$agent_folder'"
+    unzip -o "$output_dir/$agent_filename" -d "$output_dir/$agent_folder" > /dev/null 2>&1
+    rm -f "$output_dir/$agent_filename"
+
+    config_file="app-agent-config.xml"
+    echo "    Copying over $config_file..."
+    cp -f "$config_file" $output_dir/$agent_folder/ver*/conf/$config_file
+
+    echo "    Zipping '$output_dir/$agent_filename' to '$output_dir/$agent_folder'"
+    zip -r "$output_dir/$agent_filename" $output_dir/$agent_folder/* > /dev/null 2>&1
+    rm -rf "$output_dir/$agent_folder"
+
+    echo "    Updating $index_filename..."
+    sed -i "" "s,$targetUrl,$destination/$agent_filename,g" "$output_dir/$index_filename"
+
+    echo "    COMPLETED TARGET '$target'"
+
+done < "targets.txt"
